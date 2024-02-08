edb2f34cd8eb7331ddd8c35b6365b48b7b43c5e7
Shubham Kapoor <skapoor@falabella.cl>
2024-02-09 02:00:46 +0530

    first commit

---
+++ Taapman/Taapman/TaapmanApp.swift
@@ -0,0 +1,32 @@
+//
+//  TaapmanApp.swift
+//  Taapman
+//
+//  Created by skapoor on 09/02/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TaapmanApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
